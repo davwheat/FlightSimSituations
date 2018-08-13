4c0dc59517987ca645b79001dedf8fa6df863b4f
davwheat <blokmaster.help@gmail.com>
2018-08-13 04:19:13 +0100

    Initial Commit

---
+++ FlightSimSituations/FillTreeView.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace FlightSimSituations
+{
+    public class Situations
+    {
+        static readonly List<Tuple<string, string>> crewSituations = new List<Tuple<string, string>>
+        {
+            new Tuple<string, string> ( "First Officer incapacitated", "crew_fo_incapacitated" ),
+            new Tuple<string, string> ( "A member of the cabin crew feel unwell", "crew_cabin_sick" ),
+            new Tuple<string, string> ( "A member of the cabin crew has fainted", "crew_cabin_fainted" ),
+        };
+
+        static readonly List<Tuple<string, string>> emergencySituations = new List<Tuple<string, string>>
+        {
+            new Tuple<string, string> ( "Lavatory fire", "emergency_fire_lavatory" ),
+            new Tuple<string, string> ( "Samsung Galaxy Note 7 is onboard", "emergency_fire_samsung_note_seven" ),
+            new Tuple<string, string> ( "Rapid decompression", "emergency_rapid_decompression" ),
+        };
+        static readonly List<Tuple<string, string>> passengerSituations = new List<Tuple<string, string>>
+        {
+            new Tuple<string, string> ( "Pax feels unwell", "pax_unwell" ),
+            new Tuple<string, string> ( "Pax is suffering from an unknown medical emergency", "pax_medical_emergency" ),
+            new Tuple<string, string> ( "Pax is violent", "pax_violent" ),
+            new Tuple<string, string> ( "Pax is drunk", "pax_drunk" ),
+            new Tuple<string, string> ( "Pax is being abusive", "pax_abusive" ),
+            new Tuple<string, string> ( "Pax is attempting to enter the cockpit", "pax_hijack" ),
+            new Tuple<string, string> ( "Pax has died", "pax_dead" ),
+            new Tuple<string, string> ( "Pax is unconscious", "pax_unconscious" ),
+            new Tuple<string, string> ( "Pax has a weapon", "pax_armed_and_dangerous" ),
+            new Tuple<string, string> ( "Pax had a panic attack", "pax_panic_attack" ),
+        };
+
+        static readonly List<Tuple<string, string>> systemSituations = new List<Tuple<string, string>>
+        {
+            new Tuple<string, string> ( "Engine vibrations outside of safe range", "system_eng_vibrations_above_maximum" ),
+        };
+
+        public static readonly List<Tuple<string, List<Tuple<string, string>>>> allSituations = new List<Tuple<string, List<Tuple<string, string>>>>
+        {
+            new Tuple<string, List<Tuple<string, string>>> ( "Generic Emergencies", emergencySituations ),
+            new Tuple<string, List<Tuple<string, string>>> ( "Passenger", passengerSituations ),
+            new Tuple<string, List<Tuple<string, string>>> ( "Aircraft Warnings and Failures", systemSituations ),
+            new Tuple<string, List<Tuple<string, string>>> ( "Onboard Crew", crewSituations ),
+        };
+
+        public static void PopulateTreeView(TreeView tree, List<Tuple<string, List<Tuple<string, string>>>> allnodes)
+        {
+            foreach (Tuple<string, List<Tuple<string, string>>> category in allnodes)   // For every category...
+            {
+                var categoryTitle = category.Item1;                                     // Get the category name
+                var itemsInCategory = category.Item2;                                   // Get the items in the category
+
+                var tvCategory = new TreeViewItem                                       // Create a new TreeViewItem for the category parent
+                {
+                    Header = categoryTitle,
+                };
+
+                foreach (Tuple<string, string> item in itemsInCategory)                 // For each item in the category...
+                {
+                    var tvItem = new CheckBox                                           // Create a new TreeViewItem (child)
+                    {
+                        Content = item.Item1,                                           // Set the text to the item's text
+                        Name = item.Item2,                                              // Set the name to the item's ID
+                    };
+                    tvItem.Checked += delegate { MainWindow.hasChangeBeenMadeSinceLastSave = true; };
+
+                    tvCategory.Items.Add(tvItem);                                       // Add it to the category (parent)
+                }
+
+                tree.Items.Add(tvCategory);                                             // Add the category (parent) to the TreeView (root)
+            }
+        }
+    }
+}
--- FlightSimSituations/MainWindow.xaml.cs
+++ FlightSimSituations/MainWindow.xaml.cs
@@ -20,9 +20,176 @@ namespace FlightSimSituations
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static bool hasChangeBeenMadeSinceLastSave;
+
         public MainWindow()
         {
             InitializeComponent();
+            Situations.PopulateTreeView(situationlistTreeView, Situations.allSituations);
+        }
+
+        private void saveSituationConfigButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            var dlg = new Microsoft.Win32.SaveFileDialog
+            {
+                FileName = "SituationsConfig", // Default file name
+                DefaultExt = ".scfg", // Default file extension
+                Filter = "Situation Config Files (.scfg)|*.scfg" // Filter files by extension
+            };
+
+            // Show save file dialog box
+            var result = dlg.ShowDialog();
+
+            // Process save file dialog box results
+            if (result == true)
+            {
+                // Save document
+                var filename = dlg.FileName;
+                var cfgBuilder = new StringBuilder();
+
+                foreach (TreeViewItem item in situationlistTreeView.Items) // category
+                {
+                    foreach (CheckBox cb in item.Items)
+                    {
+                        if (cb.IsChecked != null)
+                        {
+                            if (cb.IsChecked.Value)
+                            {
+                                cfgBuilder.Append(cb.Name + "|1");
+                            }
+                            else
+                            {
+                                cfgBuilder.Append(cb.Name + "|0");
+                            }
+                            cfgBuilder.Append(Environment.NewLine);
+                        }
+                    }
+                }
+
+                using (var sw = new System.IO.StreamWriter(filename))
+                {
+                    sw.Write(cfgBuilder.ToString());
+                }
+            }
+            else
+            {
+                MessageBox.Show("Save cancelled!", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void loadSituationConfigButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            var dlg = new Microsoft.Win32.OpenFileDialog
+            {
+                FileName = "SituationsConfig", // Default file name
+                DefaultExt = ".scfg", // Default file extension
+                Filter = "Situation Config Files (.scfg)|*.scfg" // Filter files by extension
+            };
+
+            // Show save file dialog box
+            var result = dlg.ShowDialog();
+
+            // Process save file dialog box results
+            if (result == true)
+            {
+                // Save document
+                var filename = dlg.FileName;
+
+                var allConfigLines = System.IO.File.ReadAllLines(filename);
+                var cbCheckState = new Dictionary<string, bool?>();
+
+                foreach (var line in allConfigLines)
+                {
+                    var splitLine = line.Split('|');
+                    bool? cbState = null;
+                    try
+                    {
+                        if (splitLine[1] == "1")
+                        {
+                            cbState = true;
+                        }
+                        else if (splitLine[1] == "0")
+                        {
+                            cbState = false;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Error loading cfg: Line " + Array.IndexOf(allConfigLines, line) + " of " + allConfigLines.Length + ": no state specified for checkbox.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                        cbCheckState.Add(splitLine[0], cbState);
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        MessageBox.Show("Couldn't get value of checkbox. No string splitting char was found to separate the ID and state. Maybe your config is corrupt?");
+                        disableAllSituationsButton_Click(this, new RoutedEventArgs());
+                        return;
+                    }
+                }
+
+
+                foreach (TreeViewItem item in situationlistTreeView.Items) // category
+                {
+                    foreach (CheckBox cb in item.Items)
+                    {
+                        if (cbCheckState.TryGetValue(cb.Name, out bool? state))
+                        {
+                            cb.IsChecked = state;
+                        }
+                        else
+                        {
+                            cb.IsChecked = false;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Load cancelled!", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void enableAllSituationsButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (TreeViewItem item in situationlistTreeView.Items) // category
+            {
+                foreach (CheckBox cb in item.Items)
+                {
+                    cb.IsChecked = true;
+                }
+            }
+        }
+
+        private void disableAllSituationsButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (TreeViewItem item in situationlistTreeView.Items) // category
+            {
+                foreach (CheckBox cb in item.Items)
+                {
+                    cb.IsChecked = false;
+                }
+            }
+        }
+
+        private void ApplicationClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+            if (hasChangeBeenMadeSinceLastSave)
+            {
+                var r = MessageBox.Show(
+                        "Changes have been made to your situation configuration. Do you wish to save these changes?",
+                        "Save changes?",
+                        MessageBoxButton.YesNoCancel,
+                        MessageBoxImage.Warning
+                    );
+
+                if (r != MessageBoxResult.No)
+                    e.Cancel = true;
+
+                if (r == MessageBoxResult.Yes)
+                    saveSituationConfigButton_Click(this, new RoutedEventArgs());
+            }
         }
     }
 }
